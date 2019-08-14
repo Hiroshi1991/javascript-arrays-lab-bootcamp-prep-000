bdaf1d4c05d2b330fe86e106158ac8b944fec28e
Hiroshi S <sipahioglu_7@hotmail.com>
2019-08-14 10:38:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function prependKitten(name) {
 }
  
  function removeLastKitten() {
-   return kittens.slice(0, kittens, )
+   return kittens.slice(0, kittens, length - 1)
  }
  
 // Add your functions and code here
